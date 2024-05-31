a54e678c427bfc7a75b3e9f868b05d6b145b01d7
Shubham <prajapatishubh9881@gmail.com>
2024-05-31 12:08:14 +0530

    Logic Development Program

---
+++ operatoer-demo/src/collection/UniqueArray.java
@@ -0,0 +1,40 @@
+package collection;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueArray {
+    public static void main(String[] args) {
+        int [] array1={10,20,12,18,14};
+
+        int [] array2={12,10,16,22,12};
+
+        //create A set to store unique elements
+        Set<Integer> uniqueElements =new HashSet<>();
+
+        // Add elements from the first array
+        for(int num: array1){
+            uniqueElements.add(num);
+        }
+        //Add elements from the second Array
+        for(int num: array2)
+        {
+            uniqueElements.add(num);
+        }
+
+        //Convert the set to an array
+        int[] uniqueArray=new int[uniqueElements.size()];
+        int index=0;
+        for (int num: uniqueElements){
+            uniqueArray[index++]=num;
+
+        }
+
+        //Output the result
+        System.out.println("Output :");
+        for (int num: uniqueArray){
+            System.out.println(num+" ");
+        }
+
+    }
+}
