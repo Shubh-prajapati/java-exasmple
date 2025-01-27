c341424814f821c7d387a07972c8b8f83b20a79f
Shubham <prajapatishubh9881@gmail.com>
2025-01-27 15:10:41 +0530

    Prime Number

--- operatoer-demo/src/coffee/LogicDevelopment/PrimeNumber.java
+++ operatoer-demo/src/coffee/LogicDevelopment/PrimeNumber.java
@@ -2,30 +2,27 @@ package coffee.LogicDevelopment;
 
 public class PrimeNumber {
     public static void main(String[] args) {
-        int num=15;
-        int i;
-        if(num==1){
-            System.out.println("Its a Prime Number");
+       int num =12;
+       int i;
+       if(num==1){
+           System.out.println("It is Prime Number");
+       }
+       else {
+           for(i=2; i<num; i++)
+           {
+               if(num%1==0)
+               {
+                   break;
+               }
+           }
 
-        }
-        else {
-            for (i=2; i<num; i++)
-            {
-                if(num %i==0)
-                {
-                    break;
-                }
+           if(i==num){
+               System.out.println(num+" It is a Prime Number");
+           }
+           else {
+               System.out.println( num+" Its not a prime Number");
+           }
+       }
 
-            }
-            if(i==num)
-            {
-                System.out.println( num+ " It is a Prime Number ");
-            }
-            else{
-                System.out.println( num +" Its not a Prime Number");
-            }
-
-        }
     }
-
 }
