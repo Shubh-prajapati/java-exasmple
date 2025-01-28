9038058aa982f67f89db2a9eb79f017c4cdbe4d6
Shubham <prajapatishubh9881@gmail.com>
2025-01-28 12:53:13 +0530

    Display the Table accouding the Number

---
+++ operatoer-demo/src/coffee/LogicDevelopment/DisplayTable.java
@@ -0,0 +1,33 @@
+package coffee.LogicDevelopment;
+
+
+
+import java.util.Scanner;
+
+public class DisplayTable {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any number");
+        int input=sc.nextInt();
+
+        System.out.println("Enter which highest wanna multiply");
+        int n=sc.nextInt();
+
+        System.out.println("Your Table of :"+input);
+        int i=1,res;
+        while(i<=n){
+            res=input*i;
+            System.out.println(res);
+            i++;
+        }
+
+
+
+
+    }
+
+
+
+}
